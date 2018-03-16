1f9aa6396a25d2efdd197559f4c799bb09e1bdd3
Ryan Vanderwerf <vanderwerfr@ociweb.com>
2018-03-16 01:02:01 +0100

    fix startup bug for static db

--- complete/grails-app/init/demo/BootStrap.groovy
+++ complete/grails-app/init/demo/BootStrap.groovy
@@ -5,14 +5,16 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class BootStrap {
     def init = { servletContext ->
-        Book.saveAll(
-            new Book(name: 'Grails 3: A Practical Guide to Application Development'),
-            new Book(name: 'Falando de Grails',),
-            new Book(name: 'The Definitive Guide to Grails 2'),
-            new Book(name: 'Grails in Action'),
-            new Book(name: 'Grails 2: A Quick-Start Guide'),
-            new Book(name: 'Programming Grails')
-        )
+        if(Book.count() == 0) {
+            Book.saveAll(
+                    new Book(name: 'Grails 3: A Practical Guide to Application Development'),
+                    new Book(name: 'Falando de Grails',),
+                    new Book(name: 'The Definitive Guide to Grails 2'),
+                    new Book(name: 'Grails in Action'),
+                    new Book(name: 'Grails 2: A Quick-Start Guide'),
+                    new Book(name: 'Programming Grails')
+            )
+        }
     }
     def destroy = {
     }
